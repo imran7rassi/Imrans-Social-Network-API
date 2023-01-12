303576ce7be7c2e7446079d38c0cb6806913fe77
imran-rassi <imran.rassi.2020@gmail.com>
2023-01-12 17:04:42 -0500

    added thought.js

---
+++ models/Thought.js
@@ -0,0 +1,79 @@
+
+
+const { Schema, model, Types } = require('mongoose');
+// importing the moment module
+// for timestamp format
+const moment = require('moment')
+
+// this is the schema reaction
+const reactionSchema = new Schema (
+    {
+       reactionId: {
+        type: Schema.Types.ObjectId,
+        default: () => new Types.ObjectId(),
+       },
+       reactionBody: {
+        type: String,
+        required: true,
+        maxlength: 280
+       },
+       username: {
+        type: String,
+        required: true,
+       },
+       createdAt: {
+        type: Date,
+        default: Date.now,
+        get: createdAtVal => moment(createdAtVal).format("MMM DD, YYYY [at] hh:mm a"),
+       },
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        id: false,
+    }
+)
+
+// schema for thought
+const thoughtSchema = new Schema (
+    {
+      thoughtText: {
+        type: String,
+        required: true,
+        minlength: 1,
+        maxlength: 280,
+      },
+      createdAt: {
+        type: Date,
+        default: Date.now,
+        get: createdAtVal => moment(createdAtVal).format("MMM DD, YYYY [at] hh:mm a"),
+      },
+      username: {
+        type: String,
+        required: true,
+      },
+      reactions: [reactionSchema],
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true,
+        },
+        id: false,
+    }
+)
+
+
+// get total count of friends
+thoughtSchema.virtual('reactionCount')
+.get(function() {
+    return this.reactions.length;
+})
+
+// create the User model
+//z using the UserSchema
+const Thought = model('Thought', thoughtSchema);
+// export the Thought model
+module.exports = Thought;
