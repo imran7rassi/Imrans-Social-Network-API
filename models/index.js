bfac22dc5915a76259828c751268f3d1cc57a1c2
imran-rassi <imran.rassi.2020@gmail.com>
2023-01-13 23:18:57 -0500

    added index.js

---
+++ models/index.js
@@ -0,0 +1,7 @@
+
+// requiring the User 
+const User = require('./User');
+// requiring the Thought
+const Thought = require('./Thought');
+// exporting the both User and Thought
+module.exports = { User, Thought };
